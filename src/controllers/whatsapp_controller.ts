c29e940db05a7ea2d9e48cc95a0aff1e72e75485
Domains18 <gibsonsgibson88@gmail.com>
2024-04-02 08:16:45 +0300

    removed all files

--- src/controllers/whatsapp_controller.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from 'express';
-import { whatsappToken, phoneNumberID, metaVersion } from 'constants/variables';
-import axios from 'axios'
-import * as fs from 'fs';
-import
