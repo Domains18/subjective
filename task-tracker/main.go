fba55548a1be9b2eaa06af57079c3265da433851
Domains18 <gibsonsgibson88@gmail.com>
2025-03-08 22:32:34 +0300

    added swagger documentation

--- task-tracker/main.go
+++ task-tracker/main.go
@@ -1,7 +1,41 @@
 package main;
 
+import(
+	"github.com/gin-gonic/gin"
+	"log"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+
+// @title Task Tracker Microservice
+// @version 1.0
+// @description This is one of the larger util system for task tracking written in go
+// @termsOfService // @termsOfService URL_ADDRESS.io/terms/
+
+// @contact.name API Suppor// @termsOfService URL_ADDRESS.io/terms/
+
+// @co// @termsOfService URL_ADDRESS.io/terms/
+
+// @contact.name API Support
+// @contact.url URL_ADDRESS.swagger.io/support
 
 
 func main(){
-	
+
+
+	router:= gin.Default();
+
+	router.GET("/", func(c *gin.Context){
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	});
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+
+	if err := router.Run(":8080"); err != nil{
+		log.Fatal("Unable to start the server: %v", err);
+	}
 }
